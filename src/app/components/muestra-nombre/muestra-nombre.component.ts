712118e51a9ca63fc550b2d71bafdc62a9894e1f
Yoel Fernandez Suarez <yoel.fernandez@izertis.com>
2023-04-21 13:30:32 +0200

    fin seccion 21

---
+++ src/app/components/muestra-nombre/muestra-nombre.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-muestra-nombre',
+  templateUrl: './muestra-nombre.component.html',
+  styles: [
+  ]
+})
+export class MuestraNombreComponent implements OnChanges,OnInit {
+
+  @Input() nombre!: string;
+
+  constructor(){}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    console.log(changes);
+    
+  }
+}
